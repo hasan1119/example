a0c864932ce624a9338270a4228ef82ef727b0f7
Md. Rokibul Hasan <hasan63251119@gmail.com>
2021-10-30 20:04:09 +0600

    Completed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
 // port
@@ -30,7 +31,76 @@ async function run() {
     await client.connect();
     const database = client.db("Hotel_Booking");
     const hotel_collection = database.collection("Hotel");
-    console.log("client connected");
+    const order_collection = database.collection("Order");
+    const blog_collection = database.collection("blog");
+
+    // load all services: get api
+    app.get("/services", async (req, res) => {
+      const services = await hotel_collection.find({}).toArray();
+      res.json(services);
+    });
+
+    //add a service: post api
+    app.post("/addService", async (req, res) => {
+      const service = req.body;
+      const result = await hotel_collection.insertOne(service);
+      res.json(result);
+    });
+
+    // load a single data: get api
+    app.get("/sigleservice/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await hotel_collection.findOne({ _id: ObjectId(id) });
+      res.json(result);
+
+      // book a room
+      app.post("/book", async (req, res) => {
+        const book = req.body;
+        const result = await order_collection.insertOne(book);
+        res.json(result.insertedId);
+      });
+    });
+
+    //load a specific user's all booked room
+    app.get("/myorders/:email", async (req, res) => {
+      const email = req.params.email;
+      const services = await order_collection.find({ email }).toArray();
+      res.send(services);
+    });
+
+    // load all booked room : get api
+    app.get("/orders", async (req, res) => {
+      const services = await order_collection.find({}).toArray();
+      res.send(services);
+    });
+
+    // book a room
+    app.delete("/deletion/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await order_collection.deleteOne({ _id: ObjectId(id) });
+      res.json(result.deletedCount);
+    });
+
+    // confirmation: put api
+    app.patch("/confirmation/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateDoc = {
+        $set: {
+          status: "Confirmed",
+        },
+      };
+      const result = await order_collection.updateOne(
+        { _id: ObjectId(id) },
+        updateDoc
+      );
+      res.json(result.modifiedCount);
+    });
+
+    // confirmation: put api
+    app.get("/blogs", async (req, res) => {
+      const result = await blog_collection.find({}).toArray();
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
